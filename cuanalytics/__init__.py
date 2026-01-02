c1a60de016340aa6ca4bcb665a1bae3d986f133b
jdelano <jdelano@cedarville.edu>
2026-01-01 19:56:43 -0500

    Bump version to 0.2.0

--- cuanalytics/__init__.py
+++ cuanalytics/__init__.py
@@ -3,7 +3,7 @@ ITM 4150: Advanced Business Analytics and Visualization
 Python toolkit for course materials at Cedarville University.
 """
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 __author__ = "Dr. John D. Delano"
 
 # Import commonly used functions for convenient access
